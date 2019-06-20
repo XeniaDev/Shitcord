16abfec0901c1b6e6b6f39a3bbead1bfa11d1fbd
Aeterne Satiatus <lafayedemicheaux.luka@gmail.com>
2019-06-20 12:25:01 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,52 +14,56 @@ const msg = "Welcome to Aeterne's community!";
 Message handler.
 */
 client.on('message', message => {
-    try {
-        if (message.author.id == ownerid) {
-            var channel = message.channel;
-            var args = message.content.split(" ");
-            
-            // Spam in dm
-            if (args[0] == prefix + 'dm') {
+    if (message.author.id == ownerid) {
+        var channel = message.channel;
+        var args = message.content.split(" ");
+
+        // Spam in dm
+        if (args[0] == prefix + 'dm') {
+            delete args[0];
+            var victimid = client.users.get(args[1]);
+            delete args[1];
+            var msg = args.join(" ");
+            delete args[2];
+            client.fetchUser(victimid).then((victim) => {
+                var interval = setInterval(function () {
+                    victim.send(msg);
+                }, 600);
+            });                
+        }
+
+        // Spam multiple channels in server
+        if (args[0] == prefix + 'raid') {
+            if (args[1] != null && args[2] != null) {
                 delete args[0];
-                var victimid = client.users.get(args[1]);
+                var gid = args[1];
                 delete args[1];
                 var msg = args.join(" ");
-                delete args[2];
-                client.fetchUser(victimid).then((victim) => {
-                    var interval = setInterval(function () {
-                        victim.send(msg);
-                    }, 600);
-                });                
+                var interval = setInterval(function () {
+                        client.guilds.get(gid).channels.map(c => {
+                            if (c.type == "text") c.send(msg);
+                        });
+                }, 600);
             }
+        }
 
-            // Spam multiple channels in server
-            if (args[0] == prefix + 'raid') {
-                if (args[1] != null && args[2] != null) {
-                    delete args[0];
-                    var gid = args[1];
-                    delete args[1];
-                    var msg = args.join(" ");
-                    var interval = setInterval(function () {
-                            client.guilds.get(gid).channels.map(c => {
-                            	if (c.type == "text") c.send(msg);
-                            });
-                    }, 600);
-                }
+        // DM all members in of a server
+        if (args[0] == prefix + 'dms') {
+            if (args[1] != null && args[2] != null) {
+                delete args[0];
+                var gid = args[1];
+               delete args[1];
+                var msg = args.join(" ");
+                client.guilds.get(gid).members.map(m => {
+                    m.send(msg);
+                });
             }
-            
-            // DM all members in of a server
-            if (args[0] == prefix + 'dms') {
-                if (args[1] != null && args[2] != null) {
-                    delete args[0];
-                    var gid = args[1];
-                   delete args[1];
-                    var msg = args.join(" ");
-                    client.guilds.get(gid).members.map(m => {
-                        m.send(msg);
-                    });
-                }
-            }
+        }
+        
+        // Restart bot
+        if (args[0] == prefix + 'restart') {
+            client.destroy();
+            client.login(process.env.BOT_TOKEN);
         }
     }
 });
