69818efdf8a5e1aee3f76165fe5ac76cc0d1df9a
Aeterne Satiatus <aeternesatiatus@gmail.com>
2019-10-05 17:08:29 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 // Configuration
 const prefix = "$";
-const owner_ids = ["587487245523484674", "333012000169000960"];
+const owner_ids = ["492975557445943296"];
 
 function isOwner(id) {
     for(var i of owner_ids) {
