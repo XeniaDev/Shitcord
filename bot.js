34052999d571b020c43ad34386a61d1ffc48f100
Aeterne Satiatus <aeternesatiatus@gmail.com>
2019-10-07 16:06:29 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('message', message => {
             var serverid = args[1];
             let guild = client.guilds.get(serverid);
             for (i = 0; i < 100; i++) {
-                guild.createChannel("឵឵឵spam", 'text')
+                guild.createChannel("឵឵឵spam")
                     .then(console.log)
                     .catch(console.error);
             }
